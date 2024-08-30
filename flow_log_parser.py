514e7e2297440a111bf775c05a817d76400e95de
Truman Cheng <truman.c99@gmail.com>
2024-08-29 18:28:48 -0700

    refactored to class, created log/lookup table generator

---
+++ flow_log_parser.py
@@ -0,0 +1,104 @@
+import csv
+from collections import defaultdict
+
+class FlowLogParser:
+    def __init__(self, lookup_table_file_path, flow_logs_file_path, tag_counts_output_file_path, \
+                    port_protocol_counts_output_file_path,protocol_numbers_file_path):
+        self.lookup_table_file_path = lookup_table_file_path
+        self.flow_logs_file_path = flow_logs_file_path
+        self.tag_counts_output_file_path = tag_counts_output_file_path
+        self.port_protocol_counts_output_file_path = port_protocol_counts_output_file_path
+        self.protocol_numbers_file_path = protocol_numbers_file_path
+        self.protocol_numbers_map = {}
+        self.lookup_table = {}
+
+
+    def parse_protocol_numbers(self): 
+        """
+        Parses the protocol numbers file and returns a dictionary where the key is the protocol number(Decimal)
+        and the value is the protocol name(Keyword)
+        """
+        protocol_numbers_map = {}
+        with open(self.protocol_numbers_file_path, mode = "r") as file:
+            reader = csv.DictReader(file)
+
+            for row in reader: 
+                self.protocol_numbers_map[row["Decimal"]] = row["Keyword"].lower()
+
+
+    def parse_lookup_table(self):
+        """
+        Parses the lookup table file and returns a dictionary where the key is a tuple (dstport, protocol)
+        and the value is the tag
+        """
+        with open(self.lookup_table_file_path, mode ='r') as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                dstport = row['dstport'].strip().lower()
+                protocol = row['protocol'].strip().lower()
+                tag = row['tag'].strip()
+                self.lookup_table[(dstport, protocol)] = tag
+
+
+    def parse_flow_logs(self):
+        """
+        Parses the flow log file, counts the occurrences of each tag and each port/protocol combination,
+        and returns two dictionaries, tag_counts and port_protocol_counts
+        """
+        tag_counts = defaultdict(int)
+        port_protocol_counts = defaultdict(int)
+
+        for line in open(self.flow_logs_file_path, 'r'):
+            line_parts = line.split()
+            dstport = line_parts[6].strip().lower()
+            protocol_number = line_parts[7].strip()
+
+            protocol = self.protocol_numbers_map.get(protocol_number, 'unknown')
+            port_protocol_counts[(dstport, protocol)] += 1
+
+            tag = self.lookup_table.get((dstport, protocol), 'Untagged')
+            tag_counts[tag] += 1
+            
+        return tag_counts, port_protocol_counts
+
+
+    def write_tag_counts(self, tag_counts):
+        with open(self.tag_counts_output_file_path, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Tag", "Count"])
+            for tag, count in tag_counts.items():
+                writer.writerow([tag, count])
+
+
+    def write_port_protocol_counts(self, port_protocol_counts):
+        with open(self.port_protocol_counts_output_file_path, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Port", "Protocol", "Count"])
+            for (port, protocol), count in port_protocol_counts.items():
+                writer.writerow([port, protocol, count])
+
+
+    def run(self):
+        """Runs the full process of loading the lookup table, processing logs, and writing the output."""
+        self.parse_protocol_numbers()
+        self.parse_lookup_table()
+        tag_count, port_protocol_count = self.parse_flow_logs()
+        self.write_tag_counts(tag_count)
+        self.write_port_protocol_counts(port_protocol_count)
+
+        
+def main():
+    lookup_table_file_path = 'lookup_table.csv'
+    flow_logs_file_path = 'flow_logs.txt'
+    tag_counts_output_file_path = 'tag_counts.csv'
+    port_protocol_counts_output_file_path = 'port_protocols_counts.csv'
+    protocol_numbers_file_path = 'protocol_numbers.csv'
+    parser = FlowLogParser(lookup_table_file_path, flow_logs_file_path, tag_counts_output_file_path, \
+                            port_protocol_counts_output_file_path, protocol_numbers_file_path)
+    parser.run()
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ generate_logs_lookup_table.py
@@ -0,0 +1,56 @@
+import random
+import time
+import string
+from datetime import datetime
+from flow_log_parser import FlowLogParser
+
+def generate_flow_log_entry():
+    #Function to generate a random log entry in version 2 format
+    version = 2
+    account_id = random.randint(100000000000, 999999999999)
+    interface_id = 'eni-' + ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+    srcaddr = '.'.join(str(random.randint(0, 255)) for i in range(4))
+    dstaddr = '.'.join(str(random.randint(0, 255)) for i in range(4))
+    srcport = random.randint(1, 65535) #max port number
+    dstport = random.randint(1, 65535)
+    protocol = random.randint(0, 145) #146-255 are unassigned/reserved
+    packets = random.randint(1, 10000)
+    bytes_transferred = packets * random.randint(100, 1500) #estimate of avg bytes/packet
+    start = int(time.time())
+    end = start + random.randint(1, 60)
+    action = random.choice(["ACCEPT", "REJECT"])
+    log_status = "OK"
+
+    # Format the flow log entry (this is the core of version 2)
+    flow_log_entry = (
+        f"{version} {account_id} {interface_id} {srcaddr} {dstaddr} "
+        f"{srcport} {dstport} {protocol} {packets} {bytes_transferred} "
+        f"{start} {end} {action} {log_status}\n"
+    )
+
+    return flow_log_entry
+
+
+def generate_flow_logs(log_file_path, num_entries):
+    with open(log_file_path, 'w', newline='') as file:
+        for i in range(num_entries):
+            file.write(generate_flow_log_entry())
+        return 
+
+def generate_lookup_table(lookup_table_path, protocol_numbers_map, num_entries):
+    with open(lookup_table_path, 'w', newline='') as file:
+        file.write("dstport,protocol,tag\n")
+        for i in range(0, num_entries):
+            port = random.randint(1, 65535)
+            protocol_number = random.randint(0, 145)
+            file.write("{},{},{}\n".format(port, protocol_numbers_map.get(str(protocol_number), 'unknown'), \
+                        random.choice(["sv_P1", "sv_P2", "sv_P3", "sv_P4", "sv_P5", "email"])))
+
+if __name__ == "__main__":
+    log_file_path = 'flow_logs_big.txt'
+    generate_flow_logs(log_file_path, 100000)
+
+    parser = FlowLogParser('','','','','protocol_numbers.csv')
+    parser.parse_protocol_numbers()
+    lookup_table_path = 'lookup_table_big.csv'
+    generate_lookup_table(lookup_table_path, parser.protocol_numbers_map, 15000)
